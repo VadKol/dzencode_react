eaeaeccbac0e2366b223d01426b1862d9dbd294b
Vadym Kolomiiets <maestrovadkol@gmail.com>
2023-06-10 17:36:20 +0300

    added sessions counter

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,16 +2,21 @@ import { FC } from "react";
 
 import { TopMenu } from "@components/TopMenu";
 import { Logo } from "@components/Logo";
+import { SessionCounter } from '@components/SessionCounter/SessionCounter'
 
 export const Header: FC = () => {
   return (
     <header>
       <Logo />
-      <input
-        type="text"
-        placeholder="Search product"
-      />
+      <form>
+        <input
+          type="text"
+          placeholder="Search product"
+        />
+      </form>
+
       <TopMenu />
+      <SessionCounter />
     </header>
   );
 };
---
+++ src/components/SessionCounter/SessionCounter.tsx
@@ -0,0 +1,32 @@
+import { FC, useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:5173');
+
+export const SessionCounter: FC = () => {
+  const [activeSessions, setActiveSessions] = useState(0);
+
+  useEffect(() => {
+    socket.on('activeSessions', (count: number) => {
+      setActiveSessions(count);
+      localStorage.setItem('activeSessions', count.toString());
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    const storedSessions = localStorage.getItem('activeSessions');
+    if (storedSessions) {
+      setActiveSessions(parseInt(storedSessions));
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Sessions: {activeSessions}</h1>
+    </div>
+  );
+};
